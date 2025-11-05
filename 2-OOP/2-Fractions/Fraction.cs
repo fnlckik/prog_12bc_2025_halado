c50f988cf02a5dbffa34a5110fb971c12c925254
farkas.norbert <farkas.norbert@ckik.hu>
2025-11-05 12:08:52 +0100

    Fraction class, Simplify

---
+++ 2-OOP/2-Fractions/Fraction.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace _2_Fractions
+{
+    internal class Fraction
+    {
+        // numerator, denominator: a / b
+        private int a;
+        private int b;
+
+        public Fraction(int a, int b)
+        {
+            if (b == 0)
+            {
+                string message = $"Hiba: {a}/{b}. Nem lehet 0-val osztani.";
+                throw new DivideByZeroException(message);
+            }
+            this.a = a;
+            this.b = b;
+        }
+
+        public double Value { get => (double)a / b; }
+
+        // => 2 / 3
+        public override string ToString()
+        {
+            return $"{this.a} / {this.b}";
+        }
+
+        public static bool operator ==(Fraction x, Fraction y)
+        {
+            x = x.Simplify();
+            y = y.Simplify();
+            return x.a == y.a && x.b == y.b;
+        }
+
+        public static bool operator !=(Fraction x, Fraction y)
+        {
+            return !(x == y);
+        }
+
+        public Fraction Simplify()
+        {
+            int a = this.a;
+            int b = this.b;
+            int r = a % b;
+            while (r != 0)
+            {
+                a = b;
+                b = r;
+                r = a % b;
+            } // b: legnagyobb közös osztó
+            return new Fraction(this.a / b, this.b / b);
+        }
+    }
+}
---
+++ 2-OOP/2-Fractions/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _2_Fractions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Fraction a = new Fraction(2, 3);
+                Console.WriteLine($"Az első tört: {a}, értéke: {a.Value}");
+                Fraction b = new Fraction(3, 0);
+                Console.WriteLine($"A hibás tört: {b}.");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.WriteLine();
+
+            // Logikai operátorok
+            Fraction x = new Fraction(3, 4);
+            Fraction y = new Fraction(3, 4);
+            Console.WriteLine($"{x} == {y}: {x == y}");
+            Fraction z = new Fraction(370368, 493824);
+            Console.WriteLine($"{x} == {z}: {x == z}");
+            Console.WriteLine($"{z} => {z.Simplify()}");
+        }
+    }
+}
