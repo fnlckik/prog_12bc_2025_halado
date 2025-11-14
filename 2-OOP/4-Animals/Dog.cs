5904083ebcccefaf5752c02f00df14a8600d11a3
farkas.norbert <farkas.norbert@ckik.hu>
2025-11-14 09:41:55 +0100

    Animals - Animal, Dog

---
+++ 2-OOP/4-Animals/Dog.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _4_Animals
+{
+    // Inheritance: öröklődés
+    // Dog: leszármazott (derived class)
+    // Animal: ősosztály (base class)
+
+    // Az öröklődés olyan OOP technika,
+    // amely során a leszármazott osztály örökli 
+    // az ősosztály adattagjait, metódusait.
+
+    // A leszármazott osztálynak kötelező meghívni
+    // az ősosztály konstruktorát.
+    internal class Dog : Animal
+    {
+        public string breed; // fajta
+
+        public Dog(string name, int age, string breed) : base(name, age)
+        {
+            this.breed = breed;
+        }
+
+        public void MakeSound()
+        {
+            Console.WriteLine($"{this.name}: Vau!");
+        }
+    }
+}
---
+++ 2-OOP/4-Animals/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _4_Animals
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Példányosítások
+            Animal dumbo = new Animal("Dumbo", 70);
+            Dog ubul = new Dog("Ubul", 7, "beagle");
+            Animal scooby = new Dog("Scooby", 12, "dán dog");
+            //Dog xy = new Animal("xy", 2);
+
+            // ToString()
+            Console.WriteLine("---------- ToString() ----------");
+            Console.WriteLine(dumbo);
+            Console.WriteLine(ubul);
+            Console.WriteLine(scooby);
+            Console.WriteLine();
+
+            // MakeSound()
+            Console.WriteLine("---------- MakeSound() ----------");
+            dumbo.MakeSound();
+            ubul.MakeSound();
+            scooby.MakeSound();
+        }
+    }
+}
