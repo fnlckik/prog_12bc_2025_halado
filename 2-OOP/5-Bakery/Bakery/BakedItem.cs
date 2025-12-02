85e45c54b5e6f2ca9855c052ee1b6049dd834fd3
farkas.norbert <farkas.norbert@ckik.hu>
2025-12-02 18:12:36 +0100

    Bakery - IProduct, BakedItem

---
+++ 2-OOP/5-Bakery/Bakery/BakedItem.cs
@@ -0,0 +1,32 @@
+﻿
+namespace Bakery
+{
+    internal abstract class BakedItem : IProduct
+    {
+        protected int quantity;
+        private int price;
+
+        public BakedItem(int quantity, int price)
+        {
+            this.quantity = quantity;
+            this.price = price;
+        }
+
+        public abstract void Buy(int n);
+
+        public int GetPrice()
+        {
+            return quantity * price;
+        }
+
+        public virtual string GetCategory()
+        {
+            return "Péksütemény";
+        }
+
+        public override string ToString()
+        {
+            return $"{quantity} db - {GetPrice()} Ft";
+        }
+    }
+}
---
+++ 2-OOP/5-Bakery/Bakery/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace Bakery
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
