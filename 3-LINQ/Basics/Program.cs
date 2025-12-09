75ab283eb191c924bdf68fb187db4fa049d06971
farkas.norbert <farkas.norbert@ckik.hu>
2025-12-09 10:39:23 +0100

    LINQ - where, select

--- 3-LINQ/Basics/Program.cs
+++ 3-LINQ/Basics/Program.cs
@@ -1,15 +1,110 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Basics
 {
     internal class Program
     {
+        static void Write(string msg, IEnumerable<int> collection)
+        {
+            Console.Write(msg + " ");
+            foreach (int item in collection)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+        }
+
+        static bool IsPositive(int x)
+        {
+            return x > 0;
+        }
+
+        static IEnumerable<int> F1(IEnumerable<int> collection)
+        {
+            List<int> result = new List<int>();
+            foreach (int item in collection)
+            {
+                if (IsPositive(item))
+                {
+                    result.Add(item);
+                }
+            }
+            return result;
+        }
+
+        static bool IsEven(int x)
+        {
+            return x % 2 == 0;
+        }
+
+        static IEnumerable<int> F2(IEnumerable<int> collection)
+        {
+            List<int> result = new List<int>();
+            foreach (int item in collection)
+            {
+                if (IsEven(item))
+                {
+                    result.Add(item);
+                }
+            }
+            return result;
+        }
+
+        // Kiválogatjuk a T tulajdonságú elemeket. (Filter)
+        static IEnumerable<int> F(IEnumerable<int> collection, Func<int, bool> T)
+        {
+            List<int> result = new List<int>();
+            foreach (int item in collection)
+            {
+                if (T(item))
+                {
+                    result.Add(item);
+                }
+            }
+            return result;
+        }
+
         static void Main(string[] args)
         {
             List<int> list = new List<int> { -2, -3, 1, 1, 6, -4, 9 };
             HashSet<int> set = new HashSet<int> { 6, -7, 3, 4, 1, 23 };
             int[] array = new int[] { 5, 2, 12, 1, 3, -1, -3, 5, 24, -3 };
+
+            // 0. Írassuk ki az elemeket
+            Write("Elemek (lista):", list);
+            Write("Elemek (halmaz):", set);
+            Write("Elemek (tömb):", array);
+
+            // Pozitív elemek - kiválogatás
+            Console.WriteLine();
+            Write("Pozitív elemek (lista):", F(list, IsPositive));
+            Write("Pozitív elemek (halmaz):", F(set, IsPositive));
+            Write("Pozitív elemek (tömb):", F(array, IsPositive));
+            Write("Pozitív elemek (tömb):", array.Where(IsPositive));
+            Write("Pozitív elemek (tömb):", array.Where(x => x > 0));
+
+            // Páros elemek - kiválogatás
+            Console.WriteLine();
+            Write("Páros elemek (lista):", F(list, IsEven));
+            Write("Páros elemek (halmaz):", F(set, IsEven));
+            Write("Páros elemek (tömb):", F(array, IsEven));
+            Write("Páros elemek (tömb):", array.Where(x => x % 2 == 0));
+
+            // LINQ = Language INtegrated Query
+            // Sablon problémák => Progtétel
+            // Imperatív programozás: Hogyan oldjuk meg a problémát? (for, if, ...) -> C
+
+            // Progtételek => Sablon függvények
+            // Deklaratív programozás: Mit akarunk megoldani? (WHERE, SELECT, ...) -> SQL
+
+            Console.WriteLine();
+            // 1. Kiválogatás: Where
+            Write("10-nél nagyobb elemek:", array.Where(x => x > 10));
+
+            // 2. Másolás: Select
+            Write("Elemek négyzetei:", array.Select(x => x*x));
         }
     }
 }
