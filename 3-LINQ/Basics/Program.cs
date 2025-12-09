d0bcce32a1e481d8f472ca8b19bac38562ae4ec8
farkas.norbert <farkas.norbert@ckik.hu>
2025-12-09 09:57:43 +0100

    LINQ basics

---
+++ 3-LINQ/Basics/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Basics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int> { -2, -3, 1, 1, 6, -4, 9 };
+            HashSet<int> set = new HashSet<int> { 6, -7, 3, 4, 1, 23 };
+            int[] array = new int[] { 5, 2, 12, 1, 3, -1, -3, 5, 24, -3 };
+        }
+    }
+}
