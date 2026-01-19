aebb6c5e55fef64b663934b7b1874b28cda059e3
farkas.norbert <farkas.norbert@ckik.hu>
2026-01-19 11:41:26 +0100

    SportsCompetition - gyakorlás

--- 3-LINQ/6-SportsCompetition/Program.cs
+++ 3-LINQ/6-SportsCompetition/Program.cs
@@ -23,35 +23,62 @@ namespace SportsCompetition
             var results = SeedResults();
 
             // 1. Hány darab volt az egyes verseny típusokból?
-            
-
+            var f1 = from e in events
+                     group e by e.Type into g
+                     select new
+                     {
+                         Típus = g.Key,
+                         Mennyiség = g.Count()
+                     };
+            Print("F1: ", f1);
 
             // 2. Add meg a labdarúgás eredményét,
             // pontszám szerint csökkenő sorrendben!
-            
-
+            var f2 = from e in events
+                     join r in results on e.ID equals r.EventID
+                     where e.Name == "Labdarúgás"
+                     orderby r.Score descending
+                     select r;
+            Print("F2: ", f2);
 
             // 3. Sorold fel a neveket évfolyamonként csoportosítva!
-            
-
+            var f3 = from s in students
+                     group s.Name by s.Grade;
+            Console.WriteLine("\nF3: ");
+            foreach (var g in f3)
+            {
+                Console.WriteLine($"{g.Key}. évfolyam:");
+                foreach (var item in g)
+                {
+                    Console.WriteLine("\t" + item);
+                }
+            }
 
             // 4. Add meg, hogy Molnár Júlia milyen versenyeken 
             // vett részt és azokon hány pontot szerzett!
-            
-
+            var f4 = from r in results
+                     join s in students on r.StudentID equals s.ID
+                     join e in events on r.EventID equals e.ID
+                     where s.Name == "Molnár Júlia"
+                     select new
+                     {
+                         Verseny = e.Name,
+                         Pontszám = r.Score
+                     };
+            Print("F4: ", f4);
 
             // 5. Melyik diák hány pontot szerzett összesen a versenyek során?
-            
-            
+
+
 
             // 6. Add meg annak a diáknak a nevét, aki
             // a legkevesebb versenyen vett részt!
-            
+
 
 
             // 7. Add meg azokat a hónapokat, amikor
             // legalább két sport esemény volt!
-            
+            Console.WriteLine(events.First().Date);
 
 
             // 8. Átlagosan hány pontot szereztek atlétika versenyeken?
