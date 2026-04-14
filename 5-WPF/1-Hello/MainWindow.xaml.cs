9da4cb5f6cebd504d12531f0f913811093629efa
farkas.norbert <farkas.norbert@ckik.hu>
2026-04-14 10:53:47 +0200

    WPF CRUD

--- 5-WPF/1-Hello/MainWindow.xaml.cs
+++ 5-WPF/1-Hello/MainWindow.xaml.cs
@@ -18,11 +18,25 @@ namespace _1_Hello
     /// </summary>
     public partial class MainWindow : Window, INotifyPropertyChanged
     {
+        private string personName; // field (mező, adattag)
+
         // Binding: összekötés, összekapcsolás
         // Csak property-re lehet használni! (get, set)
         // Observable: megfigyelhető
         public ObservableCollection<string> Names { get; set; }
-        public string PersonName { get; set; }
+        
+        public string PersonName // property (tulajdonság) -> getter, setter
+        { 
+            get
+            {
+                return personName;
+            }
+            set
+            {
+                personName = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(PersonName)));
+            }
+        }
 
         public MainWindow()
         {
@@ -31,17 +45,13 @@ namespace _1_Hello
             //Names = new();
             //Names = new ObservableCollection<string>() { "Sándor", "József", "Benedek" };
             Names = ["Sándor", "József", "Benedek"];
-            PersonName = "";
+            personName = "";
             DataContext = this; // Azért kell, hogy a Binding tudja, hogy hol van a Names property.
         }
 
-        private void GreetButton_Click(object sender, RoutedEventArgs e)
-        {
-            //MessageBox.Show("Heló világ!", "Köszöntés", MessageBoxButton.OK, MessageBoxImage.Information);
-            //string name = NameTextBox.Text;
-            GreetingOutput.Text = $"Helló {PersonName}!";
-        }
+        public event PropertyChangedEventHandler? PropertyChanged;
 
+        // CREATE
         private void AddButton_Click(object sender, RoutedEventArgs e)
         {
             //string name = NameTextBox.Text;
@@ -52,12 +62,37 @@ namespace _1_Hello
             //bool? b; -> true, false, null
         }
 
-        private void NamesListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        // READ
+        private void GreetButton_Click(object sender, RoutedEventArgs e)
         {
-            // ?.ToString() -> ha null értéken hívom, akkor null legyen az eredmény
-            // null-coalescing operator
-            PersonName = NamesListBox.SelectedItem?.ToString() ?? "";
-            //NameTextBox.Text = PersonName;
+            //MessageBox.Show("Heló világ!", "Köszöntés", MessageBoxButton.OK, MessageBoxImage.Information);
+            //string name = NameTextBox.Text;
+            GreetingOutput.Text = $"Helló {PersonName}!";
         }
+
+        //private void NamesListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        //{
+        //    // ?.ToString() -> ha null értéken hívom, akkor null legyen az eredmény
+        //    // null-coalescing operator
+        //    PersonName = NamesListBox.SelectedItem?.ToString() ?? "";
+        //    //NameTextBox.Text = PersonName;
+        //}
+
+        // UPDATE
+        private void EditButton_Click(object sender, RoutedEventArgs e)
+        {
+            int i = NamesListBox.SelectedIndex;
+            if (i < 0) return;
+            Names[i] = PersonName;
+        }
+
+        // DELETE
+        private void DeleteButton_Click(object sender, RoutedEventArgs e)
+        {
+            //Names.Remove(NamesListBox.SelectedItem?.ToString() ?? "");
+            Names.Remove(PersonName);
+        }
+
+        // CRUD műveletek
     }
 }
