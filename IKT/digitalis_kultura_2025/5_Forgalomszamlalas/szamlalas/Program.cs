6612d40640f5b412b124e1189556d3b33645b91c
farkas.norbert <farkas.norbert@ckik.hu>
2026-05-26 10:04:29 +0200

    Forgalomszámlálás megoldás

--- IKT/digitalis_kultura_2025/5_Forgalomszamlalas/szamlalas/Program.cs
+++ IKT/digitalis_kultura_2025/5_Forgalomszamlalas/szamlalas/Program.cs
@@ -13,3 +13,25 @@ Console.WriteLine("2. feladat");
 Console.WriteLine($"Összesen {amount} kerékpárost számoltak.");
 
 // F3
+Console.WriteLine("\n3. feladat");
+Console.WriteLine("Óránkénti mérések:");
+int s = 0;
+for (int i = 0; i < measurements.Count; i++)
+{
+	if (measurements[i] != -1) s += measurements[i];
+	if (i % 4 == 3)
+	{
+        Console.WriteLine($"{6 + i / 4} órától {s} kerékpáros");
+		s = 0;
+	}
+}
+
+// F4
+Console.WriteLine("\n4. feladat");
+int maxValue = measurements.Max();
+int index = measurements.IndexOf(maxValue);
+int time = 6 * 60; // percben a kezdési idő
+time += (index + 1) * 15;
+int hour = time / 60;
+int min = time % 60;
+Console.WriteLine($"Az áthaladók maximális száma: {maxValue}; a rögzítés időpontja: {hour}:{min}.");
