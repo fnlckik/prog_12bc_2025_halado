d1b720d9b737d37f6bb36e2b08f86b0533f75c04
farkas.norbert <farkas.norbert@ckik.hu>
2026-06-02 09:51:18 +0200

    Digi érettségi - adatbázis

--- IKT/digitalis_kultura_2025/4_Tour_de_France/megoldas.sql
+++ IKT/digitalis_kultura_2025/4_Tour_de_France/megoldas.sql
@@ -7,3 +7,18 @@ WHERE orszag IS NOT NULL;
 SELECT ev, tav, feladok / indulok AS gyenguszok
 FROM tour
 ORDER BY feladok / indulok DESC;
+
+-- 4utolso
+SELECT TOP 1 tour.ev, gyoztesek.vnev, tour.csapat
+FROM tour INNER JOIN gyoztesek ON tour.gyoztesid = gyoztesek.vazon
+WHERE vorszag = "FRA"
+ORDER BY tour.ev DESC;
+
+-- 5tobbszor
+-- WHERE: rekordra feltétel
+-- HAVING: csoportra feltétel
+SELECT gyoztesek.vnev, COUNT(*) AS `győzelmek száma`
+FROM tour INNER JOIN gyoztesek ON tour.gyoztesid = gyoztesek.vazon
+GROUP BY gyoztesek.vazon, gyoztesek.vnev
+HAVING COUNT(*) > 1
+ORDER BY COUNT(*) DESC;
